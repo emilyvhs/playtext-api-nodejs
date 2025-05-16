692075d3edef18a97cc875a944d143ce5567f3fa
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-05-16 13:31:53 +0100

    Added play routes to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const connectToDB = require('./database/db')
+const playRoutes = require('./routes/play-routes')
 
 const app = express()
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT
 connectToDB()
 
 app.use(express.json())
+app.use('/api/plays', playRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is connected at ${PORT}`)
