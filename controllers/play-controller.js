77238697880872a37e133188cfa765761bc80cdd
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-05-16 13:47:14 +0100

    Created addNewPlays function

--- controllers/play-controller.js
+++ controllers/play-controller.js
@@ -9,7 +9,24 @@ const getSinglePlay = async (req, res) => {
 }
 
 const addNewPlay = async (req, res) => {
-    
+    try {
+        const newPlayData = req.body
+        const newlyCreatedPlay = await Play.create(newPlayData)
+
+        if(newlyCreatedPlay) {
+            res.status(201).json({
+                success : true,
+                message : 'New play added successfully',
+                data : newlyCreatedPlay
+            })
+        }
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({
+            success : false,
+            message : 'Something went wrong! Please try again'
+        })
+    }    
 }
 
 const updatePlay = async (req, res) => {
