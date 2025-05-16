7236bd147507e93cf4fe3ca532cfab318d43325c
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-05-16 13:27:21 +0100

    Setting up Play controller

---
+++ controllers/play-controller.js
@@ -0,0 +1,29 @@
+const Play = require('../models/Play')
+
+const getAllPlays = async (req, res) => {
+
+}
+
+const getSinglePlay = async (req, res) => {
+    
+}
+
+const addNewPlay = async (req, res) => {
+    
+}
+
+const updatePlay = async (req, res) => {
+    
+}
+
+const deletePlay = async (req, res) => {
+    
+}
+
+module.exports = {
+    getAllPlays,
+    getSinglePlay,
+    addNewPlay,
+    updatePlay,
+    deletePlay
+}
